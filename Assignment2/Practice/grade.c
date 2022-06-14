1778f5e0638d27a7ab54c2c7e0b0c200fb60b980
Arpan De <96812019+neo-deus@users.noreply.github.com>
2022-06-14 06:26:42 +0530

    Update grade.c

--- Assignment2/Practice/grade.c
+++ Assignment2/Practice/grade.c
@@ -6,22 +6,22 @@ int main()
     if(result>=90&&result<=100){
         printf("Grade = O");
     }
-    else if(result<90&&result>=80){
+    else if(result>=80){
         printf("Grade = E");
     }
-    else if(result<80&&result>=70){
+    else if(result>=70){
         printf("Grade = A");
     }
-    else if(result<70&&result>=60){
+    else if(result>=60){
         printf("Grade = B");
     }
-    else if(result<60&&result>=50){
+    else if(result>=50){
         printf("Grade = C");
     }
-    else if(result<50&&result>=40){
+    else if(result>=40){
         printf("Grade = D");
     }
-    else if(result<40&&result>=0){
+    else if(result>=0){
         printf("Grade = F(FAILED)");
     }
     else{
