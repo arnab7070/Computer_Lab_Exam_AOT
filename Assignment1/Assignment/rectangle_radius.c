3806bd434532f149fd9c0a93320b53c94b10683c
Arpan De <96812019+neo-deus@users.noreply.github.com>
2022-06-14 06:01:06 +0530

    Update rectangle_radius.c

--- Assignment1/Assignment/rectangle_radius.c
+++ Assignment1/Assignment/rectangle_radius.c
@@ -5,7 +5,7 @@ int main()
     int l,b,r;
     scanf("%d%d%d",&l,&b,&r);
     printf("Area of the rectangle: %d\n",l*b);
-    printf("Circumference of the rectangle: %d\n",2*(l+b));
+    printf("Perimeter of the rectangle: %d\n",2*(l+b));
     printf("Area of the cicle: %d\n",pow(r,2));
     printf("Circumference of the circle: %f\n",2*3.14*r);
     return 0;
