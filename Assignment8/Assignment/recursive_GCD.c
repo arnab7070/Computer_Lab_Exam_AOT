f80324b0b0b63416f1843b874b0023b979f1ccfd
Bishal Karmakar <69028485+BishalK007@users.noreply.github.com>
2022-06-14 20:32:54 +0530

    Update recursive_GCD.c
    braces in switch cases needed

--- Assignment8/Assignment/recursive_GCD.c
+++ Assignment8/Assignment/recursive_GCD.c
@@ -11,22 +11,23 @@ int main() {
     scanf("%d",&key);
     switch (key)
     {
-    case 1:
+    case 1:{
         int n1, n2;
         printf("Enter two positive integers: ");
         scanf("%d %d", &n1, &n2);
         printf("G.C.D of %d and %d is %d.", n1, n2, GCD(n1, n2));
         break;
-    case 2:
+    }
+    case 2:{
         int x, y, z;
         printf("Enter three positive integers: ");
         scanf("%d %d %d", &x, &y, &z);
         printf("G.C.D of %d, %d & %d is %d.", x, y, z, GCD(x, GCD(y, z)));
         break;
+    }
     default:
         printf("Please enter correct input\n");
         break;
     }
     return 0;
 }
-
