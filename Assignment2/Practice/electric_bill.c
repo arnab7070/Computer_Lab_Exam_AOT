50b7aebb529417b10c65bcc77e1979e1793ec7ac
Arpan De <96812019+neo-deus@users.noreply.github.com>
2022-06-14 06:24:58 +0530

    Update electric_bill.c

--- Assignment2/Practice/electric_bill.c
+++ Assignment2/Practice/electric_bill.c
@@ -9,13 +9,13 @@ int main()
         total_bill += unit*0.50;
     }
     else if(unit>=201&&unit<400){
-        total_bill += 200*0.50 + 100 + (unit-200)*0.65;
+        total_bill += 100 + (unit-200)*0.65;
     }
     else if(unit>=400&&unit<=600){
-        total_bill += 200*0.50 + 100 + 200*0.65 + 250 + (unit-400)*0.80;
+        total_bill += 250 + (unit-400)*0.80;
     }
     else{
-        total_bill += 200*0.50 + 100 + 200*0.65 + 250 + 400*0.85 + 425 + (unit-600)*1.25;
+        total_bill += 425 + (unit-600)*1.25;
     }
     printf("The amount to be paid: %.2f",total_bill);
     return 0;
